bf72af361c7d7a5e766a9e1c75965f5f19dc8d06
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-14 18:19:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,9 +22,10 @@ class App < Sinatra::Base
     @user_name = params[:name]
   end
   
-  # get '/multiply/:num1/:num2'
-  #   @product = params[:num1].to_i * params[:num2].to_i 
-  # end
+  get '/multiply/:num1/:num2'
+    @product = params[:num1].to_i * params[:num2].to_i
+  #{product}
+  end
   
 
 end
